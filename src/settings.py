71256a97118a1ae067d98dafc41180e5d68d80de
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2022-11-16 21:02:27 +0200

    feat: add cryptorank coin tracker

--- src/settings.py
+++ src/settings.py
@@ -8,10 +8,14 @@ load_dotenv()
 
 project_settings = {
     "TG_BOT_TOKEN": os.getenv("TG_BOT_TOKEN", None),
+    "CRYPTORANK_TOKEN": os.getenv("CRYPTORANK_TOKEN", None),
     "LOG_LEVEL": os.getenv("LOG_LEVEL", "INFO"),
     "MESSAGES_RECIPIENTS": [
         recipient for recipient in os.getenv("MESSAGES_RECIPIENTS", "").split(",") if recipient
     ],
+    "COIN_ID": int(os.getenv("COIN_ID", 0)),
+    "STARTING_COIN_BALANCE": float(os.getenv("STARTING_COIN_BALANCE", 0)),
+    "STARTING_PRICE_USD": float(os.getenv("STARTING_PRICE_USD", 0)),
 }
 
 logging.basicConfig(
